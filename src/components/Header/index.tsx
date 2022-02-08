20bae70018ff831445225573f27863ef1c5762d3
Guilherme <guilherme.devz@hotmail.com>
2022-02-07 21:09:54 -0300

    feat: completed component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,32 @@
+import { useRouter } from "next/router";
+import Image from "next/image";
+import cookieCutter from "cookie-cutter";
+
+import logo from "../../../public/images/logo.png";
+import { AiOutlineLogout } from "react-icons/ai";
+
 import styles from "./header.module.scss";
 
 export function Header() {
-  return <div></div>;
+  const router = useRouter();
+  function logOut() {
+    //deletes the token from cookies
+    cookieCutter.set("token", "", { expires: new Date(0) });
+    router.push("/");
+  }
+  return (
+    <header className={styles.container}>
+      <div className={styles.logo}>
+        <Image src={logo} alt="" />
+      </div>
+      <div>
+        <button onClick={() => logOut()}>
+          Logout
+          <span>
+            <AiOutlineLogout />
+          </span>
+        </button>
+      </div>
+    </header>
+  );
 }
